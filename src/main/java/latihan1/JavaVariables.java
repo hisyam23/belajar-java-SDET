26fc024c5aed490fe41993b5ed0274abf58f11f2
Hisyam Hasibuan <hisyam@sirka.io>
2022-09-01 22:33:13 +0700

    initial commit for remote to github

---
+++ src/main/java/latihan1/JavaVariables.java
@@ -0,0 +1,32 @@
+package latihan1;
+
+public class JavaVariables {
+    public static void main(String[] args) {
+        //integer
+        byte angka1 = 10;
+        short angka2 =11;
+        int angka3 =12;
+        long angka4 = 14L;
+
+        //floating
+        float angka5 = 10.5F;
+        double angka6 =10.6;
+
+        //other
+        boolean data1 =true;
+        char data2='H';
+
+        //casting
+
+
+        System.out.println(angka1);
+        System.out.println(angka2);
+        System.out.println(angka3);
+        System.out.println(angka4);
+        System.out.println(angka5);
+        System.out.println(angka6);
+        System.out.println(data1);
+        System.out.println(data2);
+
+    }
+}
---
+++ src/main/java/latihan1/Main.java
@@ -0,0 +1,11 @@
+package latihan1;
+
+public class Main {
+
+    //special function - main method
+    //psvm + tab
+    public static void main(String[] args) {
+        //sout+tab
+        System.out.println("Hello World!!!");
+    }
+}
